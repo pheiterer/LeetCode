9d43833f651bc8ec2ef036c678801a151aa63618
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2024-12-23 21:48:26 -0300

    Time: 5 ms (62.44%) | Memory: 45.2 MB (8.81%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,7 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
         int result = 0;
-        
+
         HashMap<Character, Integer> romanSymbols = new HashMap<>();
         romanSymbols.put('I',1);
         romanSymbols.put('V',5);
@@ -14,14 +14,8 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             int curr = romanSymbols.get(s.charAt(i));
 
-            if(i+1 == s.length()){
-                return  result += curr;
-            }
-
-            int next = romanSymbols.get(s.charAt(i+1));
-
-            if(curr < next){
-                result = result + (next - curr);
+            if(i < s.length() - 1 && curr < romanSymbols.get(s.charAt(i+1))){
+                result = result + (romanSymbols.get(s.charAt(i+1)) - curr);
                 i++;
             }else{
                result += curr;
