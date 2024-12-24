74b30649e38dafa2b9463c1535447da87b25e76a
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2024-12-24 01:29:23 -0300

    Time: 75 ms (100.00%) | Memory: 4.2 MB (5.60%) - LeetSync

--- 392-is-subsequence/is-subsequence.go
+++ 392-is-subsequence/is-subsequence.go
@@ -1,13 +1,12 @@
 func isSubsequence(s string, t string) bool {
     sSize := len([]rune(s))
-    tSize := len([]rune(t))
     if(sSize == 0){
         return true
     }
 
     j := 0
     
-	for i := 0; i < tSize; i++ {
+	for i := 0; i < len([]rune(t)); i++ {
         if (t[i] == s[j]){
             j++
             if(j == sSize){
