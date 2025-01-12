564aaa4f5d500942274891a5783ca21388e7e5bb
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-01-11 22:01:08 -0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.go
+++ 56-merge-intervals/merge-intervals.go
@@ -8,38 +8,13 @@ func merge(intervals [][]int) [][]int {
     for i := 1; i < len(intervals); i++ {
         prev := resp[len(resp)-1]
         curr := intervals[i]
-        fmt.Println(curr)
 
-
-        if ((curr[0] >= prev[0] && curr[0] <= prev[1]) ||
-        (curr[1] >= prev[0] && curr[1] <= prev[1]) ||
-        (prev[0] >= curr[0] && prev[0] <= curr[1]) ||
-        (prev[1] >= curr[1] && prev[1] <= curr[1])){
-            min := 0
-            max := 0
-            if curr[0] < prev[0] {
-                min = curr[0]
-            }else{
-                min = prev[0]
-            }
-            if curr[1] > prev[1] {
-                max = curr[1]
-            }else{
-                max = prev[1]
-            }
-            resp[len(resp)-1] = []int{min, max}
-        }else{
-            
-            resp = append(resp, curr)
-            continue
+        if prev[1] >= curr[0] {
+            resp[len(resp)-1][1] = int(math.Max(float64(curr[1]),float64(prev[1])))
+        } else {
+            resp=append(resp, curr)
         }
     }
 
-
-
-    if len(resp) == len(intervals){
-        return resp
-    }else{
-        return merge(resp)
-    }
+    return resp
 }
