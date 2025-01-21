3c067e73ebea57dad1d7a135c13f4d38c101d830
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-01-20 21:54:43 -0300

    Time: 11 ms (87.81%) | Memory: 12.4 MB (33.45%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.go
+++ 217-contains-duplicate/contains-duplicate.go
@@ -1,7 +1,8 @@
 func containsDuplicate(nums []int) bool {
     hash := make(map[int]struct{}, len(nums))
+
     for _,num := range nums{
-        if _, exist := hash[num]; exist{
+        if _, ok := hash[num]; ok{
             return true   
         }
         hash[num] = struct{}{}
