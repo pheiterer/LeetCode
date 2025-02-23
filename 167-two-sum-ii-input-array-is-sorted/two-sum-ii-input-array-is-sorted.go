3c3fd6f433345476e36ab57c66846061357ea818
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-02-23 20:48:11 -0300

    Time: 0 ms (100.00%) | Memory: 8 MB (16.37%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.go
@@ -1,16 +1,15 @@
 func twoSum(numbers []int, target int) []int {
     left := 0
     right := len(numbers)-1
-    for left < len(numbers)-1 {
-        for right > left {
-            result := numbers[left] + numbers[right]
-            if result == target {
-                return []int{left+1, right+1}
-            }
+    for left < right {
+        result := numbers[left] + numbers[right]
+        if result == target {
+            return []int{left+1, right+1}
+        } else if result < target {
+            left++
+        } else {
             right--
         }
-        left++
-        right = len(numbers)-1
     }
     return nil
 }
