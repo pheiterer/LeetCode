af7851d04192f6067c85377d50e321fe73a0fe4c
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-03-06 20:34:58 -0300

    Time: 21 ms (71.10%) | Memory: 9.4 MB (85.93%) - LeetSync

---
+++ 15-3sum/3sum.go
@@ -0,0 +1,37 @@
+func threeSum(nums []int) [][]int {
+    var result [][]int
+    sort.Ints(nums) // Ordenamos o array
+
+    for i := 0; i < len(nums)-2; i++ {
+        if nums[i] > 0 {
+            break
+        }
+        
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+
+        L, R := i+1, len(nums)-1
+        for L < R {
+            curr := nums[i] + nums[L] + nums[R]
+            if curr == 0 {
+                result = append(result, []int{nums[i], nums[L], nums[R]})
+
+                for L < R && nums[L] == nums[L+1] {
+                    L++
+                }
+                for L < R && nums[R] == nums[R-1] {
+                    R--
+                }
+                
+                L++
+                R--
+            } else if curr < 0 {
+                L++
+            } else {
+                R--
+            }
+        }
+    }
+    return result
+}
