9b5b61c5dd2112ec74089f4c190c0bcddc93aaff
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-06-10 21:24:59 -0300

    Time: 1 ms (83.13%) | Memory: 55.8 MB (25.68%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -6,10 +6,23 @@ var calPoints = function (operations) {
     let result = [];
     for(let i = 0; i < operations.length; i++){
         const curr = operations[i];
-        if (Number(curr)) result.push(Number(curr));
-        else if (curr === '+') result.push(result[result.length-1]+result[result.length-2]);
-        else if (curr === 'D') result.push(result[result.length-1]*2);
-        else if (curr === 'C') result.pop();
+
+        switch(curr) {
+            case 'D':
+                result.push(result[result.length - 1] * 2);
+                break;
+
+            case 'C':
+                result.pop();
+                break;
+
+            case '+':
+                result.push(result[result.length - 1] + result[result.length - 2]);
+                break;
+            
+            default:
+                result.push(Number(curr))
+        }
     }
 
     return result.reduce((acc, value) => acc += value, 0)
