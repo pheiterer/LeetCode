a7f4773106e18a717af73ae04fcd68f346d83c38
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-09-04 00:00:19 -0300

    Time: 10 ms (52.89%) | Memory: 58.2 MB (46.85%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,54 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+	var minEatingSpeed = function(piles, h) {
+
+	    const canFinish = (k) => {
+
+	        let hours = 0;
+
+	        for (const pile of piles) {
+
+	            hours += Math.ceil(pile / k);
+
+	        }
+
+	        return hours <= h;
+
+	    };
+
+	 
+
+	    let left = 1;
+
+	    let right = Math.max(...piles);
+
+	 
+
+	    while (left < right) {
+
+	        const mid = Math.floor((left + right) / 2);
+
+	 
+
+	        if (canFinish(mid)) {
+
+	            right = mid;
+
+	        } else {
+
+	            left = mid + 1;
+
+	        }
+
+	    }
+
+	 
+
+	    return left;
+
+	};
+
+	 
