7d4d3bbe694b2ee888071658046a8726bb04f755
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2025-12-19 01:31:49 -0300

    Time: 7 ms (67.11%) | Memory: 48 MB (72.43%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cs
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cs
@@ -2,9 +2,10 @@ public class Solution {
     public int[] SmallerNumbersThanCurrent(int[] nums) {
         int[] result = new int[nums.Length]; 
         for (int i = 0; i < nums.Length; i++){
-            for(int j = 0; j < nums.Length; j++){
+            for(int j = i; j < nums.Length; j++){
                 if (j == i) continue;
                 if (nums[j] < nums[i]) result[i]++;
+                else if (nums[j] > nums[i]) result[j]++;
             }
         }
 
